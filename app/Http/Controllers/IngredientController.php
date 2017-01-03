8d30786886a867ad0e3f6f732c50d03c80251f47
Randy Booth <Randy.Booth@gmail.com>
2017-01-03 13:54:00 -0600

    Update ingredients

--- app/Http/Controllers/IngredientController.php
+++ app/Http/Controllers/IngredientController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Ingredient;
+use App\Recipe;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Router;
+use Illuminate\Support\Facades\Cache;
 
 class IngredientController extends Controller
 {
@@ -13,7 +17,8 @@ class IngredientController extends Controller
      */
     public function index()
     {
-        //
+        $ingredients = Ingredient::whereIsRoot()->isAlcoholic()->get();
+        return view('ingredients.index')->with('ingredients', $ingredients);
     }
 
     /**
@@ -43,9 +48,75 @@ class IngredientController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show($parameters = null)
     {
-        //
+        Cache::flush();
+        $parameters_explode = explode('/', $parameters);
+        $count = 0;
+        $parent_id = null;
+        $ingredient = null;
+
+//        $test = Ingredient::withDepth()->where('slug', '=', 'rum')->having('depth', '=', 1)->get('id');
+  //        dd($test);
+        foreach($parameters_explode as $parameter) {
+//            $test = Ingredient::withDepth()->where('slug', '=', $parameter)->where('parent_id', '=', $parent_id)->having('depth', '=', $count++)->firstOrFail();
+
+            $ingredient = Cache::tags('ingredient_depth')->remember($count.'_'.$parameter, 60, function() use ($parameter, $parent_id, $count) {
+                return Ingredient::withDepth()->where('slug', '=', $parameter)->where('parent_id', '=', $parent_id)->having('depth', '=', $count)->firstOrFail();
+            });
+            $parent_id = $ingredient->id;
+            $count++;
+//            print_r($test); echo '<br><br>';
+
+//            $where = function($query) use ($parameter, $count) {
+//                return $query
+////                    ->select()
+//                    ->where('slug', '=', $parameter)
+////                    ->withDepth()->where('depth', '=', $count)
+//                    ;
+//            };
+//
+//            if ($count++) {
+//                $ingredients->orWhere($where);
+//            } else {
+//                $ingredients = Ingredient::where($where);
+//            }
+        }
+
+        dd($ingredient);
+
+        if ($count > 0 && !empty($ingredient)) {
+            $ingredients = Cache::tags('ingredients')->remember($parameters, 60, function() use ($ingredients) {
+                return $ingredients->get();
+            });
+
+            if ($ingredients->count() == count($parameters_explode)) {
+                $ingredient = $ingredients->last();
+
+                $ingredient_children = Cache::tags('ingredient_children')->remember($parameters, 60, function() use ($ingredient) {
+                    return $ingredient->children()->get();
+                });
+
+                $ingredient_descendants_id = Cache::tags('ingredient_descendants_id')->remember($parameters, 60, function() use ($ingredient) {
+                    return $ingredient->descendants()->pluck('id')->push($ingredient->id)->toArray();
+                });
+
+                $recipes = Cache::tags('ingredient_show_recipes_top')->remember($parameters, 60, function() use ($ingredient_descendants_id) {
+                    return Recipe::
+                    whereHas('ingredients', function($query) use($ingredient_descendants_id) {
+                        $query->whereIn('ingredient_recipe.ingredient_id', $ingredient_descendants_id);
+                    })
+                     ->orderBy('view_count', 'desc')
+                     ->orderBy('title')
+                     ->take(10)
+                     ->get();
+                });
+
+                return view('ingredients.show', compact('ingredient', 'ingredient_children', 'recipes', 'parameters'));
+            }
+        }
+
+        abort(404);
     }
 
     /**
--- app/Ingredient.php
+++ app/Ingredient.php
@@ -2,10 +2,61 @@
 
 namespace App;
 
+use App\Scopes\ActiveScope;
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+//use Illuminate\Support\Facades\Cache;
 use Kalnoy\Nestedset\NodeTrait;
+use Vinkla\Hashids\Facades\Hashids;
 
 class Ingredient extends Model
 {
     use NodeTrait;
+    use SoftDeletes;
+    use Sluggable;
+
+    protected $appends = ['token'];
+    protected $dates = ['deleted_at'];
+    protected $fillable = ['title'];
+    protected $hidden = ['id'];
+    protected $table = 'ingredients';
+
+    protected static function boot()
+    {
+        parent::boot();
+        static::addGlobalScope(new ActiveScope);
+    }
+
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'title',
+                'unique' => false,
+            ]
+        ];
+    }
+
+    public function getTokenAttribute()
+    {
+        return Hashids::encode($this->id);
+    }
+
+    public function scopeIsAlcoholic($query)
+    {
+        return $query->where('is_alcoholic', '=', 1);
+    }
+
+    public function recipes()
+    {
+        return $this->belongsToMany('App\Recipe')->orderBy('view_count', 'DESC')->orderBy('title');
+    }
+
+    public function newPivot(Model $parent, array $attributes, $table, $exists) {
+        if ($parent instanceof Recipe) {
+            return new IngredientRecipe($parent, $attributes, $table, $exists);
+        }
+        return parent::newPivot($parent, $attributes, $table, $exists);
+    }
 }
