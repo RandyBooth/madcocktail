bf6d7e962823f3f25e9ab1dac4790af270a47336
Randy Booth <Randy.Booth@gmail.com>
2017-01-10 16:00:07 -0600

    Clean User

--- app/User.php
+++ app/User.php
@@ -2,28 +2,22 @@
 
 namespace App;
 
-use Illuminate\Notifications\Notifiable;
+use App\Scopes\ActiveScope;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
     use Notifiable;
+    use SoftDeletes;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'email', 'password',
-    ];
+    protected $fillable = ['email', 'password'];
+    protected $hidden = ['password', 'remember_token'];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+    protected static function boot()
+    {
+        parent::boot();
+        static::addGlobalScope(new ActiveScope);
+    }
 }
