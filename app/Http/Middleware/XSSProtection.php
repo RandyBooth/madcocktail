89639babaf8b792044c6ae7f5e330c012d70ea0e
Randy Booth <Randy.Booth@gmail.com>
2017-02-09 00:39:33 -0600

    XSS replace special char, remove more than 1 space

--- app/Http/Middleware/XSSProtection.php
+++ app/Http/Middleware/XSSProtection.php
@@ -21,9 +21,16 @@ class XSSProtection
 
         $input = $request->all();
 
+        if (isset($input['title'])) {
+            $pattern = ['/(&trade;|&#8482;)/', '/(&reg;|&#174;)/', '/(&copy;|&#169;)/'];
+            $replacement = ['™', '®', '©'];
+            $input['title'] = preg_replace($pattern, $replacement, $input['title']);
+        }
+
         array_walk_recursive($input, function (&$input) {
 //            $input = trim(preg_replace('/\s+/', ' ', strip_tags($input)));
-            $input = trim(strip_tags($input));
+//            $input = trim(strip_tags($input));
+            $input = trim(preg_replace('/[[:blank:]]+/', ' ', strip_tags($input)));
         });
 
         $request->merge($input);
