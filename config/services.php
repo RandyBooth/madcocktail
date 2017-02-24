9cc9aec023d2fc90fcf839fe7fe8d6247b096509
Randy Booth <Randy.Booth@gmail.com>
2017-02-23 21:13:15 -0600

    Upgrade files from Laravel 5.4

--- config/services.php
+++ config/services.php
@@ -20,22 +20,22 @@ return [
         'domain' => env('MAILGUN_DOMAIN'),
         'secret' => env('MAILGUN_SECRET'),
     ],
-//
-//    'ses' => [
-//        'key' => env('SES_KEY'),
-//        'secret' => env('SES_SECRET'),
-//        'region' => 'us-east-1',
-//    ],
-//
-//    'sparkpost' => [
-//        'secret' => env('SPARKPOST_SECRET'),
-//    ],
-//
-//    'stripe' => [
-//        'model' => App\User::class,
-//        'key' => env('STRIPE_KEY'),
-//        'secret' => env('STRIPE_SECRET'),
-//    ],
+
+    'ses' => [
+        'key' => env('SES_KEY'),
+        'secret' => env('SES_SECRET'),
+        'region' => 'us-east-1',
+    ],
+
+    'sparkpost' => [
+        'secret' => env('SPARKPOST_SECRET'),
+    ],
+
+    'stripe' => [
+        'model' => App\User::class,
+        'key' => env('STRIPE_KEY'),
+        'secret' => env('STRIPE_SECRET'),
+    ],
 
     'facebook' => array(
         'client_id'     => '632169013530721',
