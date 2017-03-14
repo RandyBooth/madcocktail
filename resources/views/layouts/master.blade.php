a401d620a0f6716f64c98cd63cc7654938a3a48b
Randy Booth <Randy.Booth@gmail.com>
2017-03-14 10:03:18 -0500

    Schema recipe

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,11 +5,13 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
+    <meta http-equiv="content-language" content="en" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <meta property="og:site_name" content="{{ config('app.name') }}" />
     <meta property="fb:app_id" content="{{ config('services.facebook.client_id') }}" />
     <meta property="og:url" content="@yield('og-url', url()->full())" />
     {{--<meta property="og:type" content="@yield('og-type')" />--}}
--- resources/views/recipes/show.blade.php
+++ resources/views/recipes/show.blade.php
@@ -23,145 +23,150 @@
 @endif
 
 @section('content')
-    <div class="row">
-        <div class="col-12 mb-4 col-lg-7 order-lg-last col-xl-6">
-            <div id="image" class="image">
-                @php
-                    $class_blur = '';
-
-                    if (!empty($recipe_image->image)) {
-                        $image = $recipe_image->image;
-                        $class_blur = ' image-blur';
-                    } else {
-                        $image = 'blank.gif';
-                    }
-                @endphp
-                <img id="image-preview" class="image-preview card-img-top img-fluid{{$class_blur}}" data-original="{{ route('imagecache', ['template' => 'single', 'filename' => $image]) }}" src="{{ route('imagecache', ['template' => 'single-tiny', 'filename' => $image]) }}" alt="">
-
-                @if(Helper::is_owner($recipe->user_id))
-                <div id="image-loading" class="image-loading hidden-xs-up">
-                    <div class="image-loading-parent">
-                        <div class="image-loading-child">
-                            <i class="fa fa-refresh fa-spin fa-2x"></i>
-                        </div>
-                    </div>
-                </div>
-                <div id="image-edit" class="image-edit">
-
-                    <form action="{{ route('ajax_recipe_image') }}" enctype="multipart/form-data" method="POST">
-                        {{ csrf_field() }}
-
-                        <div class="hidden-xs-up">
-                            <input type="text" name="id" value="{{ $recipe->token }}">
-                            <input id="image-edit-file" type="file" name="image" class="form-control">
-                        </div>
-                        <div class="btn-group btn-group-sm" role="group" aria-label="">
-                            <button type="button" id="image-edit-change" class="btn btn-gray" href="#"><i class="fa fa-camera" aria-hidden="true"></i> @if(!empty($recipe_image->image)){!! '<span>Update' !!}@else{!! '<span class="image-edit__add">Add' !!}@endif</span> Image</button>
-                        </div>
-                    </form>
-                </div>
-                @endif
-            </div>
-        </div>
-
-        <div class="col-12 mb-4 col-lg-5 col-xl-6">
+    <div class="row h-recipe" itemscope itemtype="http://schema.org/Recipe">
+        <div class="col-12">
             <div class="row">
-                @if (!empty($recipe->title))
-                    <div class="col-12">
-                        <h1>{{ $recipe->title_sup }}</h1>
-                    </div>
-                @endif
+                <div class="col-12 mb-4 col-lg-7 order-lg-last col-xl-6">
+                    <div id="image" class="image">
+                        @php
+                            $class_blur = '';
 
-                @if (!empty($recipe->description))
-                        <div class="col-12">
-                            {!! Helper::nl2p($recipe->description, false) !!}
+                            if (!empty($recipe_image->image)) {
+                                $image = $recipe_image->image;
+                                $class_blur = ' image-blur';
+                            } else {
+                                $image = 'blank.gif';
+                            }
+                        @endphp
+                        <img id="image-preview" class="image-preview card-img-top img-fluid{{$class_blur}} u-photo" data-original="{{ route('imagecache', ['template' => 'single', 'filename' => $image]) }}" src="{{ route('imagecache', ['template' => 'single-tiny', 'filename' => $image]) }}" alt="">
+
+                        @if(Helper::is_owner($recipe->user_id))
+                        <div id="image-loading" class="image-loading hidden-xs-up">
+                            <div class="image-loading-parent">
+                                <div class="image-loading-child">
+                                    <i class="fa fa-refresh fa-spin fa-2x"></i>
+                                </div>
+                            </div>
                         </div>
-                @endif
+                        <div id="image-edit" class="image-edit">
 
-                <div class="col-12 mt-2">
-                    @php $author = (!empty($recipe_author->display_name)) ? $recipe_author->display_name : $recipe_author->username; @endphp
-                    Recipe by <a class="link" href="{{ route('user-profile.show', ['username' => $recipe_author->username]) }}">{{ $author }}</a>
+                            <form action="{{ route('ajax_recipe_image') }}" enctype="multipart/form-data" method="POST">
+                                {{ csrf_field() }}
+
+                                <div class="hidden-xs-up">
+                                    <input type="text" name="id" value="{{ $recipe->token }}">
+                                    <input id="image-edit-file" type="file" name="image" class="form-control">
+                                </div>
+                                <div class="btn-group btn-group-sm" role="group" aria-label="">
+                                    <button type="button" id="image-edit-change" class="btn btn-gray" href="#"><i class="fa fa-camera" aria-hidden="true"></i> @if(!empty($recipe_image->image)){!! '<span>Update' !!}@else{!! '<span class="image-edit__add">Add' !!}@endif</span> Image</button>
+                                </div>
+                            </form>
+                        </div>
+                        @endif
+                    </div>
+                </div>
+
+                <div class="col-12 mb-4 col-lg-5 col-xl-6">
+                    <div class="row">
+                        @if (!empty($recipe->title))
+                            <div class="col-12">
+                                <h1 itemprop="name" class="p-name">{{ $recipe->title_sup }} Recipe</h1>
+                                <meta itemprop="url" content="{{ url()->full() }}" />
+                            </div>
+                        @endif
+
+                        @if (!empty($recipe->description))
+                                <div class="col-12">
+                                    {!! Helper::nl2p($recipe->description, false) !!}
+                                </div>
+                        @endif
+
+                        <div class="col-12 mt-2">
+                            @php $author = (!empty($recipe_author->display_name)) ? $recipe_author->display_name : $recipe_author->username; @endphp
+                            Recipe by <a class="link" href="{{ route('user-profile.show', ['username' => $recipe_author->username]) }}">{{ $author }}</a>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
 
-    @if (!$ingredients->isEmpty())
-    <div class="row mb-4">
-        <div class="col-12">
-            <h3>Ingredients</h3>
+            @if (!$ingredients->isEmpty())
+            <div class="row mb-4">
+                <div class="col-12">
+                    <h3>Ingredients</h3>
 
-            <hr>
+                    <hr>
 
-            <ul class="list-unstyled">
-                @foreach($ingredients as $val)
-                    <?php
-                        $ingredient_title = $val->title;
-                        $measure_title = strtolower($val->pivot->measure_title);
-                        $measure_amount = $val->pivot->measure_amount;
-                        $measure_amount_fraction = $val->pivot->measure_amount_fraction;
-                        $string = '';
+                    <ul class="list-unstyled">
+                        @foreach($ingredients as $val)
+                            <?php
+                                $ingredient_title = $val->title;
+                                $measure_title = strtolower($val->pivot->measure_title);
+                                $measure_amount = $val->pivot->measure_amount;
+                                $measure_amount_fraction = $val->pivot->measure_amount_fraction;
+                                $string = '';
 
-                        if (!empty($ingredient_title)) {
-                            if (!empty($measure_amount_fraction)) {
-                                $string .= $measure_amount_fraction.' ';
+                                if (!empty($ingredient_title)) {
+                                    if (!empty($measure_amount_fraction)) {
+                                        $string .= $measure_amount_fraction.' ';
 
-                                if (!empty($measure_title)) {
-                                    $string .= ($measure_amount > 1 ) ? str_plural($measure_title) : str_singular($measure_title);
-                                    $string .= ' ';
-                                } else {
-                                    if ($measure_amount > 1) {
-                                        $ingredient_title = str_plural($ingredient_title);
+                                        if (!empty($measure_title)) {
+                                            $string .= ($measure_amount > 1 ) ? str_plural($measure_title) : str_singular($measure_title);
+                                            $string .= ' ';
+                                        } else {
+                                            if ($measure_amount > 1) {
+                                                $ingredient_title = str_plural($ingredient_title);
+                                            }
+                                        }
+                                    } else {
+                                        if (!empty($measure_title)) {
+                                            $string .= str_singular($measure_title) . ' of';
+                                            $string .= ' ';
+                                        }
                                     }
-                                }
-                            } else {
-                                if (!empty($measure_title)) {
-                                    $string .= str_singular($measure_title) . ' of';
-                                    $string .= ' ';
-                                }
-                            }
 
-                            if ($val->is_active) {
-                                if (isset($ingredient_slug[$val->id])) {
-                                    $ingredient_title = '<a class="link" href="'.route('ingredients.show', $ingredient_slug[$val->id]).'">'.$ingredient_title.'</a>';
-                                }
-                            }
+                                    if ($val->is_active) {
+                                        if (isset($ingredient_slug[$val->id])) {
+                                            $ingredient_title = '<a class="link" href="'.route('ingredients.show', $ingredient_slug[$val->id]).'">'.$ingredient_title.'</a>';
+                                        }
+                                    }
 
-                            $string .= $ingredient_title.' ';
-                        }
-                    ?>
-                    <li>{!! $string !!}</li>
-                @endforeach
-            </ul>
+                                    $string .= $ingredient_title.' ';
+                                }
+                            ?>
+                            <li itemprop="ingredients" class="p-ingredient">{!! $string !!}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+            @endif
+
+            @if (!empty($recipe->directions))
+            <div class="row mb-2">
+                <div class="col-12">
+                    <h3>Directions</h3>
+
+                    <hr>
+
+                    <ol class="e-instructions">
+                        @foreach($recipe->directions as $val)
+                            <li>{{ $val }}</li>
+                        @endforeach
+                    </ol>
+                </div>
+            </div>
+            @endif
+
+            @if (!empty($recipe->glass->title))
+            <div class="row mb-5">
+                <div class="col-12">
+                    Glass: {{ $recipe->glass->title }}
+                </div>
+            </div>
+            @endif
         </div>
     </div>
-    @endif
 
-    @if (!empty($recipe->directions))
-    <div class="row mb-2">
-        <div class="col-12">
-            <h3>Directions</h3>
-
-            <hr>
-
-            <ol>
-                @foreach($recipe->directions as $val)
-                    <li>{{ $val }}</li>
-                @endforeach
-            </ol>
-        </div>
-    </div>
-    @endif
-
-    @if (!empty($recipe->glass->title))
-    <div class="row mb-5">
-        <div class="col-12">
-            Glass: {{ $recipe->glass->title }}
-        </div>
-    </div>
-    @endif
-
-    <div class="row mt-4 mb-md-4">
+    <div class="row mb-md-4">
         <div class="col-12">
             <h3 class="hidden-sm-down">Share</h3>
 
